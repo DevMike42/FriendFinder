237ac8eb3aa0b70c9c8a652807cb3f7336466cf9
Mike Rivera <michaelrivera@michaels-mbp-2.attlocal.net>
2019-04-11 13:34:17 -0700

    Fixed comparison algorithm

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,10 +5,12 @@ var path = require('path');
 
 module.exports = function(app) {
 
+    // Displays survey html page
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
+    // Displays home page to any unknown url
     app.get("/*", function(req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
--- server.js
+++ server.js
@@ -8,8 +8,6 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Static files location
-// app.use(express.static("app/public"));
 
 // Router
 require("./app/routing/apiRoutes")(app);
